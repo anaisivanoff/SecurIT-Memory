4f6ffdb153cd414d8c44c41345351dad943c5ae0
ivanov <anais.ivanov@ynov.com>
2026-04-23 12:51:48 +0200

    Ajout bouttons et titre

--- ProjetSecurITMemory/ProjetSecurITMemory/Form1.Designer.cs
+++ ProjetSecurITMemory/ProjetSecurITMemory/Form1.Designer.cs
@@ -31,12 +31,16 @@
             this.components = new System.ComponentModel.Container();
             this.lblTemps = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnOptions = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblTemps
             // 
             this.lblTemps.AutoSize = true;
-            this.lblTemps.Location = new System.Drawing.Point(246, 169);
+            this.lblTemps.Location = new System.Drawing.Point(445, 398);
             this.lblTemps.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblTemps.Name = "lblTemps";
             this.lblTemps.Size = new System.Drawing.Size(62, 13);
@@ -49,15 +53,56 @@
             this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(167, 41);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(207, 29);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "SecurIT-Memory";
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(229, 95);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 23);
+            this.btnPlay.TabIndex = 2;
+            this.btnPlay.Text = "Jouer";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            // 
+            // btnOptions
+            // 
+            this.btnOptions.Location = new System.Drawing.Point(229, 124);
+            this.btnOptions.Name = "btnOptions";
+            this.btnOptions.Size = new System.Drawing.Size(75, 23);
+            this.btnOptions.TabIndex = 3;
+            this.btnOptions.Text = "Options";
+            this.btnOptions.UseVisualStyleBackColor = true;
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Location = new System.Drawing.Point(229, 153);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(75, 23);
+            this.btnQuit.TabIndex = 4;
+            this.btnQuit.Text = "Quitter";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(533, 292);
+            this.ClientSize = new System.Drawing.Size(533, 430);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnOptions);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.lblTitle);
             this.Controls.Add(this.lblTemps);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "SecurIT Memory";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -68,6 +113,10 @@
 
         private System.Windows.Forms.Label lblTemps;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnOptions;
+        private System.Windows.Forms.Button btnQuit;
     }
 }
 
