0ad250bf43a8321757f27cd45e99f865bc4ab373
Harini <harini.chandracoumar@ynov.com>
2026-04-27 12:19:24 +0200

    creation de 8 paires

--- ProjetSecurITMemory/ProjetSecurITMemory/FormGame.cs
+++ ProjetSecurITMemory/ProjetSecurITMemory/FormGame.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
+using ProjetSecurITMemory.Models;
 
 namespace ProjetSecurITMemory
 {
@@ -9,22 +10,28 @@ namespace ProjetSecurITMemory
     {
         private int tempsEcoule = 0;
         private List<Button> _boutonsCartes = new List<Button>();
+        private JeuMemory _jeu;
 
         public FormGame()
         {
             InitializeComponent();
+
+            _jeu = new JeuMemory();
+            _jeu.InitialiserJeu(8);
 
             tempsEcoule = 0;
             lblTemps.Text = "Temps : 0 s";
             timerTemps.Enabled = true;
 
             InitialiserPlateau(4, 4);
+            MettreAJourAffichageCartes();
         }
 
         private void timerTemps_Tick(object sender, EventArgs e)
         {
             tempsEcoule++;
             lblTemps.Text = $"Temps : {tempsEcoule} s";
+            _jeu.IncrementerTemps();
         }
 
         private void InitialiserPlateau(int lignes, int colonnes)
@@ -67,11 +74,73 @@ namespace ProjetSecurITMemory
 
         private void BoutonCarte_Click(object sender, EventArgs e)
         {
+            if (_jeu.BlocageClics || _jeu.PartieTerminee)
+                return;
+
             Button btn = sender as Button;
             if (btn == null)
                 return;
 
-            btn.Text = "X";
+            int index = _boutonsCartes.IndexOf(btn);
+            if (index < 0 || index >= _jeu.Cartes.Count)
+                return;
+
+            var resultat = _jeu.CliquerSurCarte(index);
+
+            MettreAJourAffichageCartes();
+
+            if (resultat == ResultatClic.DeuxiemeCarteNonCorrespondante)
+            {
+                Timer timerRetard = new Timer();
+                timerRetard.Interval = 800;
+                timerRetard.Tick += (s, args) =>
+                {
+                    timerRetard.Stop();
+                    timerRetard.Dispose();
+
+                    _jeu.RecacherCartesNonCorrespondantes();
+                    MettreAJourAffichageCartes();
+                };
+                timerRetard.Start();
+            }
+            else if (resultat == ResultatClic.Victoire)
+            {
+                timerTemps.Enabled = false;
+                MessageBox.Show(
+                    $"Bravo ! Vous avez gagné en {tempsEcoule} secondes avec {_jeu.NombreEssais} essais.",
+                    "Victoire",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information
+                );
+            }
+        }
+
+        private void MettreAJourAffichageCartes()
+        {
+            for (int i = 0; i < _boutonsCartes.Count && i < _jeu.Cartes.Count; i++)
+            {
+                var btn = _boutonsCartes[i];
+                var carte = _jeu.Cartes[i];
+
+                if (carte.Etat == EtatCarte.Cachee)
+                {
+                    btn.Text = "";
+                    btn.Enabled = true;
+                    btn.BackColor = SystemColors.Control;
+                }
+                else if (carte.Etat == EtatCarte.Revelee)
+                {
+                    btn.Text = carte.PaireId.ToString();
+                    btn.Enabled = true;
+                    btn.BackColor = Color.LightYellow;
+                }
+                else if (carte.Etat == EtatCarte.Trouvee)
+                {
+                    btn.Text = carte.PaireId.ToString();
+                    btn.Enabled = false;
+                    btn.BackColor = Color.LightGreen;
+                }
+            }
         }
     }
 }
